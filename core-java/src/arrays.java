4c394ced3b369f0ad0d12ba93009b363a7a6c50b
Hamza Najeeb <hamxa.niaxi@gmail.com>
2023-08-31 20:52:03 +0300

    declaration and basic usage of arrays

---
+++ core-java/src/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    public static void main(String[] args) {
+        int[] numArray1 = new int[5];
+        numArray1[0] = 1;
+        numArray1[1] = 2;
+        numArray1[2] = 3;
+        numArray1[3] = 4;
+        numArray1[4] = 5;
+
+        int[] numArray2 = { 3, 5, 7, 9 };
+
+        System.out.println("First value of array 1: " + numArray1[0]);
+        System.out.println("First value of array 2: " + numArray2[0]);
+    }
+}
