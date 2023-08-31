f71519526a27b95d8fb0f01be9625666e2fe5dd5
Hamza Najeeb <hamxa.niaxi@gmail.com>
2023-08-31 15:22:03 +0300

    working with variables n printing val in terminal

---
+++ core-java/src/variables.java
@@ -0,0 +1,13 @@
+
+public class variables {
+    public static void main(String[] args) {
+        int num = 5;
+        String name = "ham";
+        char letter = 'h';
+        double height = 5.7;
+        boolean human = true;
+
+        System.out.println("Participant number " + num + " namely " + name + " of height " + height
+                + " having name starting from " + letter + " is a human? " + human);
+    }
+}
