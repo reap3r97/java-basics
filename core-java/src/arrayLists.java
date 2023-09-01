4f858289558167b161e4e99fcaf63c6558c511d6
Hamza Najeeb <hamxa.niaxi@gmail.com>
2023-09-01 14:13:15 +0300

    Working with array list n convert array>list

--- core-java/src/arrayLists.java
+++ core-java/src/arrayLists.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class arrayLists {
     public static void main(String[] args) {
@@ -8,5 +10,23 @@ public class arrayLists {
         arrList.add("Xyz");
         System.out.println(arrList.get(1));
         arrList.remove(2);
+
+        // iterating through array list
+        for (int i = 0; i < arrList.size(); i++) {
+            System.out.println(arrList.get(i));
+        }
+
+        // iterating through array list using advanced for loop
+        for (String value : arrList) {
+            System.out.println(value);
+        }
+
+        // To check if array list contains the value
+        System.out.println(arrList.contains("Ham"));
+
+        // To convert array to array list
+        String[] names = { "Babu", "Rao", "Filum" };
+        List<String> nameArrayList = Arrays.asList(names);
+        System.out.println(nameArrayList.contains("Babu"));
     }
 }
